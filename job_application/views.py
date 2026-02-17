1f6ecd1742456aa89aaf5d6faf4e44a909e3dafb
Ali Aljanabi <114556000+alialjanabi12@users.noreply.github.com>
2026-02-16 18:34:55 -0800

    Creating a View and a Template

--- job_application/views.py
+++ job_application/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+#
+def index(requests):
+    """
+    HOME PAGE
+    This function is used to render the index.html file whenever the index page (http://127.0.0.1:8000/) is requested
+    """
+    return render(requests, "index.html")
