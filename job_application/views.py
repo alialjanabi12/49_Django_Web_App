93234a68bd92a2b68f21c0c8ccef45bb0938c688
Ali Aljanabi <114556000+alialjanabi12@users.noreply.github.com>
2026-02-17 18:51:22 -0800

    Creating a Form Model

--- job_application/views.py
+++ job_application/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
+from .forms import ApplicationForm
 
 
 #
-def index(requests):
+def index(request):
     """
     HOME PAGE
     This function is used to render the index.html file whenever the index page (http://127.0.0.1:8000/) is requested
     """
-    return render(requests, "index.html")
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        # form validation
+        if form.is_valid():
+            # make sure the name corresponds to the name in the html
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            occupation = form.cleaned_data["occupation"]
+            print(first_name, last_name, email, date, occupation)
+        else:
+            print(form.errors)
+
+    return render(request, "index.html")
