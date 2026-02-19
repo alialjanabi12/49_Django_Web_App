5796bb4293b0a4b3f68e0952afc816bf8c7d490a
Ali Aljanabi <114556000+alialjanabi12@users.noreply.github.com>
2026-02-18 19:38:58 -0800

    Creating an Admin Interface

--- job_application/models.py
+++ job_application/models.py
@@ -3,12 +3,11 @@ from django.db import models
 
 # Designing the database
 class Database_From(models.Model):
-    first_name = models.CharField(max_length=80)  
-    last_name = models.CharField(max_length=80) 
-    email = models.EmailField(max_length=80)  
-    date = models.DateField()  
-    occupation = models.CharField(max_length=80)  
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    email = models.EmailField(max_length=80)
+    date = models.DateField()
+    occupation = models.CharField(max_length=80)
 
-    
     def __str__(self):
-        return f"{self.first_name} {self.lastname}"
+        return f"{self.first_name} {self.last_name}"
