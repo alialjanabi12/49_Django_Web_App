21ff1d2e07d4fee19bf5442e295b6bea5e6e870d
Ali Aljanabi <114556000+alialjanabi12@users.noreply.github.com>
2026-02-18 20:01:44 -0800

    Customizing the Admin Interface

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
 from .models import Database_From
 
-admin.site.register(Database_From)
+
+class Database_FromAdmin(admin.ModelAdmin):
+    list_display = (
+        "first_name",
+        "last_name",
+        "email",
+    )
+    search_fields = (
+        "first_name",
+        "last_name",
+        "email",
+    )
+    list_filter = (
+        "date",
+        "occupation",
+    )
+    ordering = ("first_name",)
+    # make one field 'read only'
+    readonly_fields = ("occupation",)
+
+
+admin.site.register(Database_From, Database_FromAdmin)
